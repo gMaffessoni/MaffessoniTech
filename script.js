f9943aaf328ca3dbac8262ddf69a898e27d4f932
Guilherme Maffessoni <maffessonig@gmail.com>
2024-09-01 23:38:28 -0300

    alterações gerais

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleNavbar() {
+    var navbarCollapse = document.querySelector('.navbar-collapse');
+    navbarCollapse.classList.toggle('active');
+}
+
+function toggleSidebar() {
+    var sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('active');
+}
